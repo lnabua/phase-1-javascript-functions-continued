939d36c5086441c50501ad270a9b829011573990
lnabua <nabua.lorraine@gmail.com>
2021-02-24 23:36:53 -0800

    commit tree 24c7d4a67de4debeda71ab4fb2e4a60b82d22709

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Your code here
+function saturdayFun(saturdayActivity = "roller-skate") {
+    return `This Saturday, I want to ${saturdayActivity}!`;
+}
+
+const mondayWork = function(mondayActivity = "go to the office") {
+    return `This Monday, I will ${mondayActivity}.`;
+}
+
+
+function wrapAdjective(flair = "*") { 
+    return function(param) {
+        console.log(`You are ${flair}${param}${flair}!`);
+    }
+}
+wrapAdjective()("a hard worker")
+wrapAdjective("||")("a dedicated programmer")
